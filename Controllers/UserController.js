b2e816c77e71bb463d41949bd298a162fe03f96e
Venktesh Soma <venkteshs@cybage.com>
2021-06-04 10:03:17 +0530

    Added the checkin and checkout field in database

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -7,8 +7,8 @@ exports.registerUser = async (request, response) => {
     firstName: request.body.firstName,
     lastName: request.body.lastName,
     email: request.body.email,
-    // checkIn: request.body.checkIn,
-    // checkOut: request.body.checkOut,
+    checkIn: request.body.checkIn,
+    checkOut: request.body.checkOut,
   });
 
   console.log("User:", user);
@@ -20,7 +20,8 @@ exports.registerUser = async (request, response) => {
         .status(400)
         .json({ message: "User already exists with this email" });
     } else {
-      response.status(201).json({ message: "User Created Successfully!" });
+      // response.status(201).json({ message: "User Created Successfully!" });
+      response.status(201).json(user);
     }
   });
 };
@@ -35,4 +36,38 @@ exports.getSingleUser = async (request, response) => {
     console.log("Error:", error);
     response.status(400).json({ message: "Can't fetch user by provided data" });
   }
+};
+
+// Put request
+exports.updateUserDetails = async (request, response, next) => {
+  const userId = request.params.id;
+  console.log("User id: ", userId);
+  const firstName = request.body.firstName;
+  const lastName = request.body.lastName;
+  const email = request.body.email;
+  const checkIn = request.body.checkIn;
+  const checkOut = request.body.checkOut;
+  const checkInDate = new Date(checkIn);
+  const checkOutDate = new Date(checkOut);
+
+  await userModel
+    .findByIdAndUpdate(
+      { _id: userId },
+
+      {
+        $set: {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          checkIn: checkInDate,
+          checkOut: checkOutDate,
+        },
+      }
+    )
+    .then(() => {
+      userModel.findOne({ _id: userId }).then((user) => {
+        response.send(user);
+        console.log("Updated user", user);
+      });
+    });
 };
